43f258669eb6867d58892e785a6e5ebf74e04a9c
Jordy Tejada <jtejadala@unsa.edu.pe>
2023-06-25 21:14:17 -0500

    Editando atributos de la clase node

--- AVLTree.java
+++ AVLTree.java
@@ -2,25 +2,19 @@ import java.util.NoSuchElementException;
 
 public class AVLTree<T extends Comparable<T>> {
 
-    class Node {
-        // No es necesario que Node tenga <T> Ya que esta clase (Node) está inculuida en
-        // una clase genérica
-        protected T data;
-        protected Node left;
-        protected Node right;
-        protected int fb; // Factor de balance
+    private class Node {
+        private String data;
+        private int key;
+        private Node left;
+        private Node right;
+        private int height;
 
-        public Node(T data, Node left, Node right) {
+        public Node(String data) {
             this.data = data;
-            this.left = left;
-            this.right = right;
-            this.fb = 0;
-        }
-
-        // Para los nuevos nodos hoja (No tienen hijos)
-        public Node(T data) {
-            // Llamamos al constructor
-            this(data, null, null);
+            this.key = calculateKey(data);
+            this.left = null;
+            this.right = null;
+            this.height = 1;
         }
     }
 
